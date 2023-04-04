15aff19f4eca1dd16ac54cb41d87eca1f2d5295b
SurenderKM2105 <85401086+SurenderKM2105@users.noreply.github.com>
2023-04-04 19:12:53 +0530

    Update Data types.c

--- Data types.c
+++ Data types.c
@@ -1,12 +1,36 @@
 #include<stdio.h>
+
 int main()
+
 {
+
 int a=10;
-printf("%d",a);// prints 10
+
+printf("%d \n",a);// prints 10
+
 int a1=010;
 
-printf("%d",a1);// prints decimal equivalent of octal number 10 , result is 8
+printf("%d \n",a1);// prints decimal equivalent of octal number 10 , result is 8
+
 int b=0x41;
-printf("%d",b);// prints decimal equivalent of hexadecimal 41, which is 65
+
+printf("%d \n",b);// prints decimal equivalent of hexadecimal 41, which is 65
+
+int c= 12.5;
+
+printf("%d \n",c);// 12 :  truncates digits after decimal point
+
+int d= 12.4;
+
+int e= 12.9;
+
+printf("%d \n  %d \n",d,e);// 12 12 
+
+ // this will not round off...
+
+ 
+
 return 0;
+
 }
+
